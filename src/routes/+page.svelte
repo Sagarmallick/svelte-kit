7eb7c3a85be6b7b7ba32f0e67ec69f796231521f
Sagar_Mallick <sagarmallick711@gmail.com>
2024-04-27 14:15:46 +0530

    (commit-7): add navigation programmaticaly by adding button in home page and use goto function to redirection to another page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,19 @@
+<script>
+    import {goto,beforeNavigate, afterNavigate} from '$app/navigation'
+    const handleClick=()=>{
+        console.log('placing your order')
+        goto('/products')
+    }
+    beforeNavigate((navigation)=>{
+        console.log({before:navigation})
+    })
+    afterNavigate((navigation)=>{
+        console.log({after:navigation})
+    })
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Deep Dive in Svelte Concepts</p>
 <a href="/blog">blog</a>
+
+<button on:click={handleClick}>place order</button>
