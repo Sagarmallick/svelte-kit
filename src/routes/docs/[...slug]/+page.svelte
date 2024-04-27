58cbc3364db1334a4491dae0927c9530fedf5bdf
Sagar_Mallick <sagarmallick711@gmail.com>
2024-04-27 13:04:03 +0530

    (commit-4) add catch all segment by add docs folder inroutes and under init add [...slug folder convention for catch dynamic routes]

---
+++ src/routes/docs/[...slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import {page} from '$app/stores'
+    const {slug}=$page.params
+    console.log(slug)
+    let slugArr=slug.split('/')
+    console.log(slugArr)
+</script>
+
+{#if slugArr.length===1}
+<h2>Viewing docs for feature {slugArr[0]}</h2>
+{:else if slugArr.length===2}
+<h2>Viewing docs for feature {slugArr[0]} and concept {slugArr[0]}</h2>
+{/if}
