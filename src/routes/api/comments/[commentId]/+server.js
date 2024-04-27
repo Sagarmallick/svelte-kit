5a468a1fafc4f03d346b0aca3fdb63a7e6a939cb
Sagar_Mallick <sagarmallick711@gmail.com>
2024-04-27 22:59:29 +0530

    (commit-17):create local server by npm i serve-json package add script in package.json serve-json and add db.json file which serve by serve-json

--- src/routes/api/comments/[commentId]/+server.js
+++ src/routes/api/comments/[commentId]/+server.js
@@ -17,3 +17,12 @@ export async function PATCH(requestEvent){
     patchComment.text=text
     return json(patchComment)
 }
+
+export async function DELETE(requestEvent){
+    const {params}=requestEvent
+    const {commentId}=params
+    const deleteComment=comments.find((comment)=>comment.id===parseInt(commentId))
+    const deleteIndex=comments.findIndex((comment)=>comment.id===parseInt(commentId))
+    comments.splice(deleteIndex,1)
+    return json(deleteComment)
+}
--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,4 +1,1 @@
 <h1>products list</h1>
-<h2>product 1</h2>
-<h2>product 2</h2>
-<h2>product 3</h2>
