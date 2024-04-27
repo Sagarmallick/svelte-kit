ff807fc1e06625a711e0719072f855b98997bcad
Sagar_Mallick <sagarmallick711@gmail.com>
2024-04-27 18:15:05 +0530

    (commit-9):add route layout by add in routes root folder +layout file and add slot tag which pretend to be a proxy of home page or a child

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<header class="header-section">header</header>
+<slot/>
+<footer class="footer-section">footer</footer>
+
+<style>
+.header-section ,.footer-section{
+    background-color: black;
+    color: orange;
+    margin: 2rem 0;
+    padding: 1rem 0.5rem;
+
+}
+</style>
