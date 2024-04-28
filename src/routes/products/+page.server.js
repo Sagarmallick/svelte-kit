6ef2b67bea294c84d5abb8cc6ae07d1c7c3e48c7
Sagar_Mallick <sagarmallick711@gmail.com>
2024-04-28 14:32:12 +0530

    (commit-20):add universal load function by adding +page.server.js in products folder this run load function only at server end not in browser end by  adding serverLoadEvent init

--- src/routes/products/+page.server.js
+++ src/routes/products/+page.server.js
@@ -1,5 +1,6 @@
-export const load = async (loadEvent) => {
-    const {fetch}=loadEvent
+export const load = async (serverLoadEvent) => {
+    console.log('Loading function called in page.server.js')
+    const {fetch}=serverLoadEvent
     const title = 'list of available products'
     const response=await fetch('http://localhost:4000/products')
     const products= await response.json()
