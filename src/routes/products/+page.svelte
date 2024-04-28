904ddf6192dae0666c66ff5acc3d188eb04d4aca
Sagar_Mallick <sagarmallick711@gmail.com>
2024-04-28 15:19:19 +0530

    (commit-21):add server loader function  in products page which is +page.server.js it  handles the data  +page.js in other hand is universal loader which handle the data and component constructor which passes to page.svelte

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,14 +1,11 @@
 <script>
-   export let data;
-   const products=data.products
+	export let data;
+	const products = data.products;
+    const Component = data.Component
 </script>
+
 <h1>products list</h1>
 <h2>{data.title}</h2>
 {#each products as product (product)}
-<div>
-    <h2>{product.title}</h2>
-    <p>{product.description}</p>
-    <hr/>
-</div>
+	<Component {product} />
 {/each}
-
---
+++ src/routes/products/product.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let product
+</script>
+<div>
+    <h2>{product.title}</h2>
+    <p>{product.description}</p>
+    <hr/>
+</div>
