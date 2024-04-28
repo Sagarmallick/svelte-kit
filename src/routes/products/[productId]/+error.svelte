5db226fac93471776eb5586b6716f499fb3bb900
Sagar_Mallick <sagarmallick711@gmail.com>
2024-04-28 23:24:30 +0530

    (commit-23): handle error when we hitproduct/5 which is not exsist by adding if condition in +page.server.js in [productId] and add +error.svelte file to display the error

---
+++ src/routes/products/[productId]/+error.svelte
@@ -0,0 +1,6 @@
+<script>
+    import {page} from '$app/stores'
+</script>
+
+<h2>{$page.status} {$page.error.message}</h2>
+<p>{$page.error.hint}</p>
--- src/routes/products/[productId]/+page.server.js
+++ src/routes/products/[productId]/+page.server.js
@@ -1,10 +1,16 @@
-export const load = async(serverLoadEvent)=>{
-    const {params,fetch}=serverLoadEvent
-    const {productId}=params
-    const response=await fetch(`http://localhost:4000/products/${productId}`)
-    const product= await response.json()
+import { error } from "@sveltejs/kit"
 
-    return{
+export const load = async (serverLoadEvent) => {
+    const { params, fetch } = serverLoadEvent
+    const { productId } = params
+    if (productId > 4) {
+        throw error(404, { message: 'oh no! look like product not currently available', hint: 'try a different product' })
+    }
+
+    const response = await fetch(`http://localhost:4000/products/${productId}`)
+    const product = await response.json()
+
+    return {
         product
     }
 }
