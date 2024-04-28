362e0e91ee1648bc5356dc075b318a630fd73056
Sagar_Mallick <sagarmallick711@gmail.com>
2024-04-28 12:15:46 +0530

    (commit-18):add fetch data to product page my add +age.js in products folder to handle fetch data for products while fetch function in +page.js not recommended to use window.fetch method insteed it recommended fetch method from  loadEvent

---
+++ src/routes/products/+page.js
@@ -0,0 +1,10 @@
+export const load = async (loadEvent) => {
+    const {fetch}=loadEvent
+    const title = 'list of available products'
+    const response=await fetch('http://localhost:4000/products')
+    const products= await response.json()
+    return {
+        title,
+        products
+    }
+}
--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,1 +1,14 @@
+<script>
+   export let data;
+   const products=data.products
+</script>
 <h1>products list</h1>
+<h2>{data.title}</h2>
+{#each products as product (product)}
+<div>
+    <h2>{product.title}</h2>
+    <p>{product.description}</p>
+    <hr/>
+</div>
+{/each}
+
