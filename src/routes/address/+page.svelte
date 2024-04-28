66283cd7dfdaca09bf01831e6180f0014a566868
Sagar_Mallick <sagarmallick711@gmail.com>
2024-04-28 13:51:32 +0530

    (commit-19): add api fetch for adress page by first add postcode object in db.json the fetch db.json postcode by api/postcodes end point which we universally fetch then in routes/address page we fetch data from universally api end point in address +page.js file then display adress which is fetch by adress +page.js which is fetch from universal api end point

---
+++ src/routes/address/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    let emptyAddress=''
+    export let data 
+    const {addressList}=data 
+</script>
+
+<h1>select your address</h1>
+
+<select bind:value={emptyAddress}>
+    <option value="">pick an address</option>
+        {#each addressList as mapAddress}
+        <option value={mapAddress}>{mapAddress}</option>
+        {/each}
+    
+</select>
